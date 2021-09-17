675adf006c91fa3e63fa90fe4028e212ac8e2465
Kevin Cainday <Kevin.Cainday@ey.com>
2021-09-17 18:38:36 +0800

    tasd

---
+++ WebApplication1/Controllers/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class MenuController: Controller
+    {
+        public IActionResult Index()
+        {
+            var menu = new Menu()
+            {
+                Message = "Hello"
+            };
+            ViewBag.MyMessage = menu.Message;
+            ViewData["MyMessage"] = menu.Message;
+            return View(menu);
+        }
+    }
+}
